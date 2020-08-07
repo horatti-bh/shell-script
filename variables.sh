ec74cfacfbc01531854fd2aef00c5b0706ef332d
horatti-bh <horatti123>
2020-08-07 19:30:09 +0530

    check data check

--- variables.sh
+++ variables.sh
@@ -5,5 +5,16 @@ MSG="Hello word"
 echo "$MSG"
 echo ${MSG}
 
-teamsize=7
-echo number of teams = ${teamsize}s
+Teamsize=7
+echo number of teams = ${Teamsize}s
+
+
+
+a=abc
+b=10
+c=true
+d=10.9
+e=abc123
+
+DATA=$(date +%F)
+echo "Hello good morning date is $DATE "
