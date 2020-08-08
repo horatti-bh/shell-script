2ab7cedb96ca365777b651b0aef8c17e331f8216
horatti-bh <horatti123>
2020-08-08 19:35:34 +0530

    Checking functions

--- function.sh
+++ function.sh
@@ -7,3 +7,14 @@ function test() {
 name=BTH
 test
 echo "I am good to move $location"
+
+
+test2() {
+  echo "1st arguments $1"
+  echo "2nd arguments $2"
+  echo "ll arguemnts $*"
+  echo "Total arguments $#"
+
+}
+
+test2 bb nummi yammi zummi
