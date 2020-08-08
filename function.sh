a0e990e2def7a945e83e43f5ed15b51e4a3ad97e
horatti-bh <horatti123>
2020-08-08 19:27:44 +0530

    Checking functions

--- function.sh
+++ function.sh
@@ -1,7 +1,7 @@
 function test() {
     echo "Hello function, How are you?"
     echo "My name is $name"
-    echo "I am good to move for $location"
+    echo "I am good to move $location"
 }
 name=BTH
 location=AnyWhere
