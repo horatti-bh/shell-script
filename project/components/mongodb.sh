b6db167c3d1b0ff60a79632f4c6c8930c1f1cef1
horatti-bh <horatti123>
2020-08-09 23:35:16 +0530

    Project code added

--- project/components/mongodb.sh
+++ project/components/mongodb.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 source components/common.sh
-info  "hello monagodb"
+info  "Setting up MongoDB"
+info "Setting up YUM repository"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' > /etc/yum.repos.d/mongodb-org-4.2.repo
+
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+  else
+    echo ERROR
+    fi
