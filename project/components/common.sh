75cd52750844ebd726a6f23e7f1734c5d8ae33aa
horatti-bh <horatti123>
2020-08-09 23:26:12 +0530

    Project code added

---
+++ project/components/common.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+info() {
+  echo "[${B}INFO${N}] $1"
+}
+
--- project/components/mongodb.sh
+++ project/components/mongodb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "hello monagodb"
+source components/common.sh
+info  "hello monagodb"
