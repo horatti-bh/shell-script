099520e0fa56d46361446f81f72d2bbe01c41292
horatti-bh <horatti123>
2020-08-10 22:07:16 +0530

    shell script for mysql

--- project/components/mysql.sh
+++ project/components/mysql.sh
@@ -8,7 +8,9 @@ yum list installed | grep mysql-community-server &> /dev/null
 if [[ $? -eq 0 ]]; then
   STAT $? "Finish: Skipping Package is already installed"
   else
-     wget -qO- https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar | tar -x
+    if [[ ! -f /tmp/mysql-community-server-5.7.28-1.el7.x86_64.rpm ]]; then
+ wget -qO- https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar | tar -x
+fi
  STAT $? "Finish: Finished downloading MYSQL"
  info "start: Installing the MYSQL"
 yum install mysql-community-client-5.7.28-1.el7.x86_64.rpm \
