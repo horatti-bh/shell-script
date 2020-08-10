cbb753f4ee7b749c03c7324d18ba6ef60a6135cd
horatti-bh <horatti123>
2020-08-10 21:47:43 +0530

    shell script for mysql

--- project/components/mysql.sh
+++ project/components/mysql.sh
@@ -4,5 +4,10 @@ info  "Setting up MYSQL"
 
 info "start: Downloading MYSQL"
 cd /tmp
- wget -qO- https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar | tar -x
+yum list installed | grep mysql-community-server &> /dev/null
+if [[ $? -eq 0 ]]; then
+  STAT $? "Finish: Skipping Package is already installed"
+  else
+     wget -qO- https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar | tar -x
  STAT $? "Finish: Finished downloading MYSQL"
+  fi
