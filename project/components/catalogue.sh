40d8c591e82e80358d4f3fb4dc75e25e91f94e3e
horatti-bh <horatti123>
2020-08-11 18:31:01 +0530

    shell script for catalogue

--- project/components/catalogue.sh
+++ project/components/catalogue.sh
@@ -30,7 +30,7 @@ STAT $? "Finish: Permissions to application"
 
 info "start: Setup catalogue systemD service"
 # mkdir -p /var/log/robo-shop/
-envsubst < /home/${APP_USER}/${service}/${service}-ss.service  > /etc/systemd/system/${service}.service &>> ${LOG_FILE}
+envsubst < /home/${APP_USER}/${service}/${service}.service  > /etc/systemd/system/${service}.service &>> ${LOG_FILE}
 STAT $? "Finish: Setup catalogue systemD service"
 
 info "start: Start catalogue service"
