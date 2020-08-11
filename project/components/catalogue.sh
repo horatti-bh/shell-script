05b106edb80d2225fee069d03b540938a0826730
horatti-bh <horatti123>
2020-08-11 15:47:39 +0530

    shell script for catalogue

--- project/components/catalogue.sh
+++ project/components/catalogue.sh
@@ -7,7 +7,6 @@ source components/common.sh
 info  "Setting up catalogue micro service"
 info "start: Install NodeJS"
 InstallNodeJS
-curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/nodejs/install.sh | bash &>> ${LOG_FILE}
 STAT $? "Finish: Installed NodeJS"
 info "start: adding application user"
 createAppuser ${APP_USER}
