cb5c9a4d43ec73f203db51f4e82462913500123a
horatti-bh <horatti123>
2020-08-11 21:20:59 +0530

    shell script for ratings

--- project/components/ratings.sh
+++ project/components/ratings.sh
@@ -25,3 +25,23 @@ yum-config-manager --enable remi-php73 &>> ${LOG_FILE}
 yum -y install php php-opcache php73-php-pdo composer &>> ${LOG_FILE}
 STAT $? "Finish: Installed PHP"
 
+info "start: updated port number"
+sed -i '/^Listen/ c Listen 7004' /etc/httpd/conf/httpd.conf &>> ${LOG_FILE}
+STAT $? "Finish: Updated port number"
+
+info "start: Clone repo and update HTDOCS"
+CLONE_TMP ratings &>> ${LOG_FILE}
+cp /tmp/ratings/status.conf /etc/httpd/conf.d/ &>> ${LOG_FILE}
+rm -rf /var/www/html &>> ${LOG_FILE}
+cp -r /tmp/ratings/html /var/www/html &>> ${LOG_FILE}
+STAT $? "Finish: Clone repo and updated"
+
+info "start: Install PHP dependencies"
+cd /var/www/html
+composer install &>> ${LOG_FILE}
+STAT $? "Finish: Installed PHP dependencies"
+
+info "start: Start the HTTPD Service"
+systemctl enable httpd &>> ${LOG_FILE}
+systemctl start httpd &>> ${LOG_FILE}
+STAT $? "Finish: Started the HTTPD Service"
