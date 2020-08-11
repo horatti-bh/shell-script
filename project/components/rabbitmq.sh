476710a79ebe5759049aa6e221d5fe685cdba268
horatti-bh <horatti123>
2020-08-11 15:00:16 +0530

    shell script for RABBITMQ

--- project/components/rabbitmq.sh
+++ project/components/rabbitmq.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 source components/common.sh
 info  "Setting up RABBITMQ"
+
+info "start: Install Erlang"
+yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y &>> ${LOG_FILE}
+STAT $? "Finish: Installed Erlang"
+
+info "start: Setting up yum repo"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> ${LOG_FILE}
+STAT $? "Finish: Setup yum repo"
+
+info "start: Installing RABBITMQ"
+ yum install rabbitmq-server -y &> ${LOG_FILE}
+ STAT $? "Finish: Installed RabbitMQ"
+
+  info "Start: Start the rabbitmq Service"
+  systemctl enable rabbitmq-server  &>/dev/null
+  systemctl start rabbitmq-server &>> ${LOG_FILE}
+  STAT $? "Finish: Started rabbitmq service"
+
