4bd5141a68e5ca4ae92a3effeccd6a16adaec1eb
horatti-bh <horatti123>
2020-08-11 18:50:08 +0530

    shell script for user

--- project/components/cart.sh
+++ project/components/cart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export service=user
+export service=cart
 export APP_USER=roboshop
 export APP_HOME=/home/${APP_USER}/${service}
 
 source components/common.sh
-info  "Setting up user micro service"
+info  "Setting up cart micro service"
 info "start: Install NodeJS"
 InstallNodeJS
 STAT $? "Finish: Installed NodeJS"
@@ -14,9 +14,9 @@ info "start: adding application user"
 createAppuser ${APP_USER}
 STAT $? "Finish: created application user"
 
-info "start: Clone the user repo"
+info "start: Clone the cart repo"
 CLONE_APP ${service} ${APP_USER} &> ${LOG_FILE}
-STAT $? "Finish: Clone the user repo"
+STAT $? "Finish: Clone the cart repo"
 
 info "start: install NodeJS dependencies"
 cd ${APP_HOME}
@@ -27,15 +27,15 @@ info "start: Permissions to application"
 chown ${APP_USER}:${APP_USER} ${APP_HOME} -R
 STAT $? "Finish: Permissions to application"
 
-info "start: Setup user systemD service"
+info "start: Setup cart systemD service"
 # mkdir -p /var/log/robo-shop/
-envsubst < /home/${APP_USER}/${service}/${service}-ss.service  > /etc/systemd/system/${service}.service &>> ${LOG_FILE}
-STAT $? "Finish: Setup catalogue systemD service"
+envsubst < /home/${APP_USER}/${service}/${service}.service  > /etc/systemd/system/${service}.service &>> ${LOG_FILE}
+STAT $? "Finish: Setup cart systemD service"
 
-info "start: Start user service"
+info "start: Start cart service"
 systemctl daemon-reload &>> ${LOG_FILE}
-systemctl enable user &>> ${LOG_FILE}
-systemctl start user &>> ${LOG_FILE}
-STAT $? "Finish: Start user service"
+systemctl enable cart &>> ${LOG_FILE}
+systemctl start cart &>> ${LOG_FILE}
+STAT $? "Finish: Start cart service"
 
 
