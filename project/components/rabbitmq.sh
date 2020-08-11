7d83a3d86fd7bfe59de944a6e43d52a5dc03063d
horatti-bh <horatti123>
2020-08-11 15:17:20 +0530

    shell script for RABBITMQ

--- project/components/rabbitmq.sh
+++ project/components/rabbitmq.sh
@@ -5,6 +5,7 @@ info  "Setting up RABBITMQ"
 info "start: Install Erlang"
 yum list installed | grep esl-erlang &>/dev/null
 if [[ $? -eq 0 ]]; then
+  STAT $? "Finish: Installed Erlang"
   else
 yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y &>> ${LOG_FILE}
 fi
