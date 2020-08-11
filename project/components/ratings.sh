2d0777e0eb729bb9a7a9c73e6fd9d5b3c212f8e3
horatti-bh <horatti123>
2020-08-11 21:27:46 +0530

    shell script for ratings

--- project/components/ratings.sh
+++ project/components/ratings.sh
@@ -7,7 +7,14 @@ export APP_HOME=/home/${APP_USER}/${service}
 source components/common.sh
 info  "Setting up ratings micro service"
 
-
+info "start: setup HTTPD Web Server"
+yum list install | grep remi-release-7 &>>/dev/null
+if [[ $? -eq 0 ]]; then
+  STAT $? "Finish: Done HTTPD Web Server"
+  else
+    yum install  http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>> ${LOG_FILE}
+     STAT $? "Finish: Done rpms HTTPD Web Server"
+  fi
   info "start: Install Apach web server"
 yum install httpd -y &>> ${LOG_FILE}
 yum -y install yum-utils -y &>> ${LOG_FILE}
