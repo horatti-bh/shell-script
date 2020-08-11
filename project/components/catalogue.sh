3fb8b9a776b866a263a75040b5b55373499d3637
horatti-bh <horatti123>
2020-08-11 15:35:53 +0530

    shell script for catalogue

--- project/components/catalogue.sh
+++ project/components/catalogue.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source components/common.sh
-info  "Setting up catalogue"
-info "start: Setting up YUM repository"
+info  "Setting up catalogue micro service"
+info "start: Install NodeJS"
+InstallNodeJS
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/nodejs/install.sh | bash &>> ${LOG_FILE}
+STAT $? "Finish: Installed NodeJS"
+
--- project/components/common.sh
+++ project/components/common.sh
@@ -36,6 +36,21 @@ STAT() {
     mkdir -p /tmp/${COMPONENT}
     git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/horatti-bh/${COMPONENT}.git /tmp/${COMPONENT}
    }
+   InstallNodeJS() {
+     if [[ -e "/bin/node" ]]; then
+       return 0
+       fi
+       URL=$(curl -s https://nodejs.org/en/download/ | xargs -n1 | grep linux-x64.tar | sed -e 's|=| |g' -e 's|>| |g'| xargs -n1 | grep ^http)
+       FILENAME=$(echo $URL | awk -F / '{print $NF}')
+       FOLDER_NAME=$(echo $FILENAME |sed -e 's/.tar.xz//')
+curl -s -o /tmp/$FILENAME $URL
+cd /opt
+tar -xf /tmp/$FILENAME
+mv $FOLDER_NAME nodejs
+ln -s /opt/nodejs/bin/node /bin/node || true
+ln -s /opt/nodejs/bin/npm /bin/npm  || true
+ln -s /opt/nodejs/bin/npx /bin/npx || true
+   }
 
 
 #  if [ $? -eq 0 ]; then
